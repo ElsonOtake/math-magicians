bc3c802135c149b681a31fcfce6c85304db568df
Elson Otake <elsonotake@yahoo.com.br>
2022-06-29 13:29:35 -0300

    Add KeyButton component

---
+++ src/components/KeyButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class KeyButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { keyValue } = this.props;
+    return (
+      <button type="button">
+        {keyValue}
+      </button>
+    );
+  }
+}
+
+KeyButton.defaultProps = {
+  keyValue: '',
+};
+
+KeyButton.propTypes = {
+  keyValue: PropTypes.string,
+};
+
+export default KeyButton;
