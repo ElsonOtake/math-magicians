f640454add185f9de8d06d5486d54426be7c3f84
Elson Otake <elsonotake@yahoo.com.br>
2022-06-29 16:46:06 -0300

    Update props to array

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -18,16 +18,11 @@ class Calculator extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <KeyboardRow col1="AC" col2="±" col3="%" col4="÷" />
-          <KeyboardRow col1="7" col2="8" col3="9" col4="x" />
-          <KeyboardRow col1="4" col2="5" col3="6" col4="-" />
-          <KeyboardRow col1="1" col2="2" col3="3" col4="+" />
-          <KeyboardLastRow col1="0" col2="•" col3="=" />
-          {/* <tr>
-            <td colSpan="2">0</td>
-            <td>•</td>
-            <td className="orange">=</td>
-          </tr> */}
+          <KeyboardRow data={['AC', '+/-', '%', '÷']} />
+          <KeyboardRow data={['7', '8', '9', 'x']} />
+          <KeyboardRow data={['4', '5', '6', '-']} />
+          <KeyboardRow data={['1', '2', '3', '+']} />
+          <KeyboardLastRow data={['0', '.', '=']} />
         </tbody>
       </table>
     );
