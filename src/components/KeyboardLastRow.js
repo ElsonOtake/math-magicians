280e933766e0e454cfd858e4b5b574a1e6ebdbb7
Elson Otake <elsonotake@yahoo.com.br>
2022-06-30 17:09:41 -0300

    Update refactor from class to function

--- src/components/KeyboardLastRow.js
+++ src/components/KeyboardLastRow.js
@@ -1,35 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import KeyButton from './KeyButton';
 
-class KeyboardLastRow extends Component {
-  constructor(props) {
-    super(props);
-    this.handleStateChange = this.handleStateChange.bind(this);
-  }
-
-  handleStateChange(calc) {
-    const { handleStateChange } = this.props;
-    handleStateChange(calc);
-  }
-
-  render() {
-    const { data, calc } = this.props;
-    return (
-      <tr>
-        <td colSpan="2">
-          <KeyButton handleStateChange={this.handleStateChange} calc={calc} keyValue={data[0]} />
-        </td>
-        <td>
-          <KeyButton handleStateChange={this.handleStateChange} calc={calc} keyValue={data[1]} />
-        </td>
-        <td className="orange">
-          <KeyButton handleStateChange={this.handleStateChange} calc={calc} keyValue={data[2]} />
-        </td>
-      </tr>
-    );
-  }
-}
+const KeyboardLastRow = ({ handleStateChange, data, calc }) => (
+  <tr>
+    <td colSpan="2">
+      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[0]} />
+    </td>
+    <td>
+      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[1]} />
+    </td>
+    <td className="orange">
+      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[2]} />
+    </td>
+  </tr>
+);
 
 KeyboardLastRow.defaultProps = {
   data: [],
