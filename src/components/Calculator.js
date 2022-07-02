f8ebada2a6e06d954edb422f671c6759e86446cf
Elson Otake <elsonotake@yahoo.com.br>
2022-07-02 14:09:50 -0300

    Update using React Context

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import './Calculator.css';
 import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
+
+const UserContext = createContext();
 
 const Calculator = () => {
   const [calc, setCalc] = useState({
@@ -12,38 +14,41 @@ const Calculator = () => {
 
   const handleStateChange = (newCalc) => setCalc(newCalc);
 
-  const { next, total, operation } = calc;
+  const { total, next, operation } = calc;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className="total" colSpan="4">{ next || operation || total || 0 }</th>
-        </tr>
-      </thead>
-      <tbody>
-        <KeyboardRow handleStateChange={handleStateChange} calc={calc} data={['AC', '+/-', '%', '÷']} />
-        <KeyboardRow handleStateChange={handleStateChange} calc={calc} data={['7', '8', '9', 'x']} />
-        <KeyboardRow handleStateChange={handleStateChange} calc={calc} data={['4', '5', '6', '-']} />
-        <KeyboardRow handleStateChange={handleStateChange} calc={calc} data={['1', '2', '3', '+']} />
-        <KeyboardLastRow handleStateChange={handleStateChange} calc={calc} data={['0', '.', '=']} />
-      </tbody>
-    </table>
+    <UserContext.Provider value={calc}>
+      <table>
+        <thead>
+          <tr>
+            <th className="total" colSpan="4">{ next || operation || total || 0 }</th>
+          </tr>
+        </thead>
+        <tbody>
+          <KeyboardRow handleStateChange={handleStateChange} data={['AC', '+/-', '%', '÷']} />
+          <KeyboardRow handleStateChange={handleStateChange} data={['7', '8', '9', 'x']} />
+          <KeyboardRow handleStateChange={handleStateChange} data={['4', '5', '6', '-']} />
+          <KeyboardRow handleStateChange={handleStateChange} data={['1', '2', '3', '+']} />
+          <KeyboardLastRow handleStateChange={handleStateChange} data={['0', '.', '=']} />
+        </tbody>
+      </table>
+    </UserContext.Provider>
+
   );
 };
 
-const KeyboardRow = ({ handleStateChange, data, calc }) => (
+const KeyboardRow = ({ handleStateChange, data }) => (
   <tr>
     <td>
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[0]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[0]} />
     </td>
     <td>
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[1]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[1]} />
     </td>
     <td>
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[2]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[2]} />
     </td>
     <td className="orange">
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[3]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[3]} />
     </td>
   </tr>
 );
@@ -51,25 +56,23 @@ const KeyboardRow = ({ handleStateChange, data, calc }) => (
 KeyboardRow.defaultProps = {
   data: [],
   handleStateChange: () => {},
-  calc: {},
 };
 
 KeyboardRow.propTypes = {
   data: PropTypes.arrayOf(PropTypes.string),
   handleStateChange: PropTypes.func,
-  calc: PropTypes.objectOf(PropTypes.string),
 };
 
-const KeyboardLastRow = ({ handleStateChange, data, calc }) => (
+const KeyboardLastRow = ({ handleStateChange, data }) => (
   <tr>
     <td colSpan="2">
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[0]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[0]} />
     </td>
     <td>
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[1]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[1]} />
     </td>
     <td className="orange">
-      <KeyButton handleStateChange={handleStateChange} calc={calc} keyValue={data[2]} />
+      <KeyButton handleStateChange={handleStateChange} keyValue={data[2]} />
     </td>
   </tr>
 );
@@ -77,36 +80,35 @@ const KeyboardLastRow = ({ handleStateChange, data, calc }) => (
 KeyboardLastRow.defaultProps = {
   data: [],
   handleStateChange: () => {},
-  calc: {},
 };
 
 KeyboardLastRow.propTypes = {
   data: PropTypes.arrayOf(PropTypes.string),
   handleStateChange: PropTypes.func,
-  calc: PropTypes.objectOf(PropTypes.string),
 };
 
-const KeyButton = ({ keyValue, handleStateChange, calc }) => (
-  <button
-    type="button"
-    onClick={(event) => {
-      handleStateChange(calculate(calc, event.target.textContent));
-    }}
-  >
-    {keyValue}
-  </button>
-);
+const KeyButton = ({ keyValue, handleStateChange }) => {
+  const calc = useContext(UserContext);
+  return (
+    <button
+      type="button"
+      onClick={(event) => {
+        handleStateChange(calculate(calc, event.target.textContent));
+      }}
+    >
+      {keyValue}
+    </button>
+  );
+};
 
 KeyButton.defaultProps = {
   keyValue: '',
   handleStateChange: () => {},
-  calc: {},
 };
 
 KeyButton.propTypes = {
   keyValue: PropTypes.string,
   handleStateChange: PropTypes.func,
-  calc: PropTypes.objectOf(PropTypes.string),
 };
 
 export default Calculator;
