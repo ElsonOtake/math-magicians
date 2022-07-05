b0e9c382b44b9e256a560a215d25f4ddeb6d2f9f
Elson Otake <elsonotake@yahoo.com.br>
2022-07-05 18:19:12 -0300

    Update Routes for each page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calc" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
