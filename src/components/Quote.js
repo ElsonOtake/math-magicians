1539c1b12a16354507717cd25be87196b57a74ff
Elson Otake <elsonotake@yahoo.com.br>
2022-07-05 18:49:15 -0300

    Update new div

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Quote = () => (
-  <div>
-    {`
-      Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.
-      — William Paul Thurston, American mathematician
-    `}
-  </div>
+  <>
+    <div>
+      {`
+        Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.
+      `}
+    </div>
+    <div>
+      {`
+        — William Paul Thurston, American mathematician
+      `}
+    </div>
+  </>
+
 );
 
 export default Quote;
