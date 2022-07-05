2e91302677c29ddf6ade587c658a39fca2e8b2b0
Elson Otake <elsonotake@yahoo.com.br>
2022-07-05 18:37:52 -0300

    Update Layout Route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import App from './components/Calculator';
 import Home from './components/Home';
 import Quote from './components/Quote';
@@ -10,9 +11,11 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quote" element={<Quote />} />
-        <Route path="/calc" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="/calculator" element={<App />} />
+        </Route>
       </Routes>
     </Router>
   </React.StrictMode>,
