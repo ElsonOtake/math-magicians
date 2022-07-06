83cb0708e5cf843b02224e5cd2bad58faad1221c
Elson Otake <elsonotake@yahoo.com.br>
2022-07-06 19:19:25 -0300

    Add tests for Quote component

---
+++ src/__tests__/Quote.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Quote from '../components/Quote.js';
+
+describe('Quote', () => {
+  test('renders Quote component', () => {
+    render(<Quote />);
+    expect(screen.getByText('Refresh your screen to read a new quote')).toBeInTheDocument();
+  });
+});
