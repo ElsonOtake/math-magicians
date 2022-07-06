f836ed714165e6eb48f733809a23cfea9df16510
Elson Otake <elsonotake@yahoo.com.br>
2022-07-06 20:29:39 -0300

    Add tests for NoPage component

---
+++ src/__tests__/NoPage.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import NoPage from '../components/NoPage.js';
+
+describe('NoPage', () => {
+  test('renders NoPage component', () => {
+    render(<NoPage />);
+    expect(screen.queryByText(/404/)).toBeTruthy();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeVisible();
+    expect(screen.debug()).toMatchSnapshot();
+  });
+});
