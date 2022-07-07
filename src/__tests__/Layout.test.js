685564ac24e32c51cccd8f7a7d35f14618213212
Elson Otake <elsonotake@yahoo.com.br>
2022-07-07 13:27:18 -0300

    Update test description

--- src/__tests__/Layout.test.js
+++ src/__tests__/Layout.test.js
@@ -4,23 +4,28 @@ import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from '../components/Layout';
 
-describe('Layout', () => {
-  test('renders Layout component', () => {
-    render(<Layout />, { wrapper: BrowserRouter });
-    expect(screen.getByRole('navigation')).toBeInTheDocument();
-    expect(screen.getByRole('navigation')).toBeVisible();
-    expect(screen.getByRole('navigation')).not.toHaveClass();
-    expect(screen.getByRole('heading')).toBeInTheDocument();
-    expect(screen.getByRole('heading')).toBeVisible();
-    expect(screen.getByRole('heading')).not.toHaveClass();
-    expect(screen.getByRole('heading')).toHaveTextContent('Math Magicians');
-    expect(screen.getByRole('list')).toBeInTheDocument();
-    expect(screen.getByRole('list')).toBeVisible();
-    expect(screen.getByRole('list')).not.toHaveClass();
-    expect(screen.getByText(/Math Magicians/)).toBeTruthy();
-    expect(screen.getByText(/Home/)).toBeTruthy();
-    expect(screen.getByText(/Calculator/)).toBeTruthy();
-    expect(screen.getByText(/Quote/)).toBeTruthy();
+describe('Tests for the Layout component', () => {
+  render(<Layout />, { wrapper: BrowserRouter });
+    test('Checks for the following tests on screen', () => {
+      expect(screen.getByText(/Math Magicians/)).toBeTruthy();
+      expect(screen.getByText(/Home/)).toBeTruthy();
+      expect(screen.getByText(/Calculator/)).toBeTruthy();
+      expect(screen.getByText(/Quote/)).toBeTruthy();
+
+      expect(screen.getByRole('navigation')).toBeInTheDocument();
+      expect(screen.getByRole('navigation')).toBeVisible();
+      expect(screen.getByRole('navigation')).not.toHaveClass();
+
+      expect(screen.getByRole('heading')).toBeInTheDocument();
+      expect(screen.getByRole('heading')).toBeVisible();
+      expect(screen.getByRole('heading')).not.toHaveClass();
+      expect(screen.getByRole('heading')).toHaveTextContent('Math Magicians');
+
+      expect(screen.getByRole('list')).toBeInTheDocument();
+      expect(screen.getByRole('list')).toBeVisible();
+      expect(screen.getByRole('list')).not.toHaveClass();
+  });
+  test('Checks for the Snapshot', () => {
     expect(screen.debug()).toMatchSnapshot();
   });
 });
