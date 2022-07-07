cbc6fcfa63515fc811e2304d59702fbb6a167921
Elson Otake <elsonotake@yahoo.com.br>
2022-07-06 23:46:17 -0300

    Update new tests

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -8,10 +8,13 @@ describe('Calculator', () => {
     render(<Calculator />);
     expect(screen.getByRole('table')).toBeInTheDocument();
     expect(screen.getByRole('table')).toBeVisible();
+    expect(screen.getByRole('table')).not.toHaveClass();
     expect(screen.queryByText(/Todo List React/)).toBeNull();
     expect(screen.getByText(/Let's do some math!/)).toBeTruthy();
     expect(screen.getByRole('heading')).toBeInTheDocument();
     expect(screen.getByRole('heading')).toBeVisible();
+    expect(screen.getByRole('heading')).not.toHaveClass();
+    expect(screen.getByRole('heading')).toHaveTextContent("Let's do some math!");
     const total = screen.getByRole('columnheader');
     fireEvent.click(screen.getByText('5'));
     expect(screen.debug(total)).toMatchSnapshot();
