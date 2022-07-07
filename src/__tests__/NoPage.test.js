a7d48eed7d24dc5a070f5086ef3d9cc70eb2fcac
Elson Otake <elsonotake@yahoo.com.br>
2022-07-07 13:29:24 -0300

    Update tests

--- src/__tests__/NoPage.test.js
+++ src/__tests__/NoPage.test.js
@@ -5,8 +5,13 @@ import NoPage from '../components/NoPage';
 
 describe('Tests for NoPage component', () => {
   render(<NoPage />);
-  test('Check for the text 404 on screen', () => {
+  test('Check for the following tests on screen', () => {
     expect(screen.queryByText(/404/)).toBeTruthy();
+    
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeVisible();
+    expect(screen.getByRole('heading')).not.toHaveClass();
+    expect(screen.getByRole('heading')).toHaveTextContent('404');
   });
   test('Check for the snapshot of debug', () => {
     expect(screen.debug()).toMatchSnapshot();
