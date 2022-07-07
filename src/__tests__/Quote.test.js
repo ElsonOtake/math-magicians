cab971aa1dc5ee82b46a521871adefe5a2dd3102
Elson Otake <elsonotake@yahoo.com.br>
2022-07-07 13:08:22 -0300

    Update test description

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -3,9 +3,9 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Quote from '../components/Quote';
 
-describe('Quote', () => {
-  test('renders Quote component', () => {
-    render(<Quote />);
+describe('Tests for the Quote component', () => {
+  render(<Quote />);
+  test('Check for the following text on screen', () => {
     expect(screen.getByText('Refresh your screen to read a new quote')).toBeInTheDocument();
     expect(screen.getByText('Refresh your screen to read a new quote')).toBeVisible();
     expect(screen.getByText('Refresh your screen to read a new quote')).not.toHaveClass();
