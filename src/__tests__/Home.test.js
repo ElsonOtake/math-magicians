f4060db0027f82f5a10bf7745afaf2133ba3d792
Elson Otake <elsonotake@yahoo.com.br>
2022-07-07 13:34:02 -0300

    Update tests description

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -3,12 +3,14 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../components/Home';
 
-describe('Home', () => {
-  test('renders Home component', () => {
-    render(<Home />);
+describe('Tests for the Home component', () => {
+  render(<Home />);
+  test('Check for the following test on screen', () => {
     expect(screen.getByText('Welcome to our page')).toBeInTheDocument();
     expect(screen.getByText(/Wikipedia/)).toBeInTheDocument();
     expect(screen.getByText(/Digitopolis/)).toBeInTheDocument();
+  });
+  test('Check for the snapshot', () => {
     expect(screen.debug()).toMatchSnapshot();
   });
 });
