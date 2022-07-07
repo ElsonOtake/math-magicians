fd1e1f1b2d3b768b5e784954aeead1b5d678e8e0
Elson Otake <elsonotake@yahoo.com.br>
2022-07-06 23:27:56 -0300

    Add tests for Layout component

---
+++ src/__tests__/Layout.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout.js';
+
+describe('Layout', () => {
+  test('renders Layout component', () => {
+    render(<Layout />, {wrapper: BrowserRouter});
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeVisible();
+    expect(screen.getByRole('navigation')).not.toHaveClass();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeVisible();
+    expect(screen.getByRole('heading')).not.toHaveClass();
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByRole('list')).toBeVisible();
+    expect(screen.getByRole('list')).not.toHaveClass();
+    expect(screen.getByText(/Math Magicians/)).toBeTruthy();
+    expect(screen.getByText(/Home/)).toBeTruthy();
+    expect(screen.getByText(/Calculator/)).toBeTruthy();
+    expect(screen.getByText(/Quote/)).toBeTruthy();
+    expect(screen.debug()).toMatchSnapshot();
+  });
+});
